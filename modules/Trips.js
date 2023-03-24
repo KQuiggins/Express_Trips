7779633f02afe597d575e4e2400cc3fd7398f449
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2023-03-24 15:02:58 -0400

    added dotenv package

--- modules/Trips.js
+++ modules/Trips.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/travel');
+//mongoose.connect('mongodb://127.0.0.1:27017/travel');
+require('dotenv').config()
+const uri = process.env.MONGODB_URI
+//const test_uri = process.env.PLAYGROUND_URI
+// Connect to the MongoDB Atlas cluster
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
 
 const tripSchema = new mongoose.Schema({
     driver: {type: String, required: true},

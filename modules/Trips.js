45eda496768f55b8ccf382699ec1ce060ba6ba1f
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2023-03-27 14:44:46 -0400

    reset changes to schema

--- modules/Trips.js
+++ modules/Trips.js
@@ -1,34 +1,29 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
-//mongoose.connect('mongodb://127.0.0.1:27017/travel');
+require('dotenv').config();
 
-require('dotenv').config()
-
-const uri = process.env.MONGODB_URI
-//const test_uri = process.env.PLAYGROUND_URI
-//const compass_uri = process.env.COMPASS_URI
+const uri = process.env.MONGODB_URI;
 
 // Connect to the MongoDB Atlas cluster
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas!');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 const tripSchema = new mongoose.Schema({
-    driver: {type: String, required: true},
-    date: {type: String, required: true},
-    city: {type: String, required: true},
-    miles: {type: Number, required: true},
-    gallons: {type: Number, required: true},
+  driver: {type: String, required: true},
+  date: {type: String, required: true},
+  city: {type: String, required: true},
+  miles: {type: Number, required: true},
+  gallons: {type: Number, required: true},
 });
 
 tripSchema.virtual('milesPerGallon').get(function() {
-    return this.miles / this.gallons;
-  });
+  return this.miles / this.gallons;
+});
 
 const TripsModel = mongoose.model('trips', tripSchema);
 
-module.exports = TripsModel
+module.exports = TripsModel;
